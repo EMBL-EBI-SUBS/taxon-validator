eb35bc3e9f45f05cd2f4fc45ae62e7c6291d8d9a
Flavia Penim <flaviapenim@gmail.com>
2017-06-13 16:57:40 +0100

    Added rabbitMQ eager initialization - queue declaration.

--- src/main/java/uk/ac/ebi/subs/validator/taxon/TaxonomyValidatorApplication.java
+++ src/main/java/uk/ac/ebi/subs/validator/taxon/TaxonomyValidatorApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "uk.ac.ebi.subs.validator")
 public class TaxonomyValidatorApplication {
 
     public static void main(String[] args) {
