9d8823c29b01732118cf116baeb6fdc5a683cead
Flavia Penim <flaviapenim@gmail.com>
2017-07-26 15:27:09 +0100

    Add caching to getTaxonById method.

--- src/main/java/uk/ac/ebi/subs/validator/taxon/core/TaxonomyService.java
+++ src/main/java/uk/ac/ebi/subs/validator/taxon/core/TaxonomyService.java
@@ -1,6 +1,7 @@
 package uk.ac.ebi.subs.validator.taxon.core;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,6 +13,7 @@ public class TaxonomyService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Cacheable("taxonomies")
     public Taxonomy getTaxonById(String id) {
         return restTemplate.getForObject(ENA_TAXONOMY_SERVICE + "tax-id/" + id, Taxonomy.class);
     }
