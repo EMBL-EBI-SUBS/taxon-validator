77c751e0570bd26edf9b5403bb35792c44ee94dc
Karoly Erdos <karoly@ebi.ac.uk>
2017-10-03 14:12:22 +0100

    Remove not used getter methods for taxon

--- src/main/java/uk/ac/ebi/subs/validator/taxon/core/TaxonomyService.java
+++ src/main/java/uk/ac/ebi/subs/validator/taxon/core/TaxonomyService.java
@@ -17,14 +17,4 @@ public class TaxonomyService {
     public Taxonomy getTaxonById(String id) {
         return restTemplate.getForObject(ENA_TAXONOMY_SERVICE + "tax-id/" + id, Taxonomy.class);
     }
-
-    public Taxonomy getTaxonByScientificName(String scientificName) {
-        Taxonomy[] array = restTemplate.getForObject(ENA_TAXONOMY_SERVICE + "scientific-name/" + scientificName, Taxonomy[].class);
-        return array[0];
-    }
-
-    public Taxonomy getTaxonByTaxonomicName(String taxonomicName) {
-        Taxonomy[] array = restTemplate.getForObject(ENA_TAXONOMY_SERVICE + "any-name/" + taxonomicName, Taxonomy[].class);
-        return array[0];
-    }
 }
