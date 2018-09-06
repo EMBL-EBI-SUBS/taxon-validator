f5fe075bff140ca3799e9fd9d8103cbb5388dfea
David Richardson <davidr@ebi.ac.uk>
2018-09-06 11:07:00 +0100

    undo work on wrong branch

--- src/main/java/uk/ac/ebi/subs/validator/taxon/messaging/TaxonValidatorRoutingKeys.java
+++ src/main/java/uk/ac/ebi/subs/validator/taxon/messaging/TaxonValidatorRoutingKeys.java
@@ -5,5 +5,5 @@ public class TaxonValidatorRoutingKeys {
     public static final String EVENT_VALIDATION_SUCCESS = "validation.success";
     public static final String EVENT_VALIDATION_ERROR = "validation.error";
 
-    static final String EVENT_TAXON_SAMPLE_VALIDATION = "taxonomy.sample.validation";
+    static final String EVENT_TAXON_SAMPLE_VALIDATION = "taxon.sample.validation";
 }
